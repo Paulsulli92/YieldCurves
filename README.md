792fdabdd8930983c0d338f31411ec9618057b90
Paul Sullivan <paulsullivan92@comcast.net>
2021-01-07 14:40:01 -0600

    Add files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # YieldCurves
+A simple app for viewing US Treasury yield data.  It shows the yields for different bond durations for the current day an several other times up to a year ago.
+
+I have this hosted in AWS using a S3 static website.  The website used AJAX to call an API that we host to get the data, then populates a HighCharts object with the data.  To set this up you must:
+- Create a Lambda function and upload the Python script to it.  This script pulls the latest data from the Treasury's website and then returns data for the days we want to display
+- Create an API Gateway with an endpoint that invokes the lambda
+- Replace API_ID in chart.html with the ID of the API Gateway you created
+- Create a bucket, upload the two .html files to it and enable it as a static website
---
+++ yields.py
@@ -0,0 +1,93 @@
+import csv, time, sys, pprint, shutil, json
+
+from botocore.vendored import requests
+import xml.etree.ElementTree as ET
+
+from datetime import datetime, date, timedelta
+
+
+class YieldData(object):
+    def __init__(self):
+        self.days = {}
+
+    
+    def pullData(self, url):
+        # Query the 
+        response = requests.get(url, stream=True)
+        tree = ET.ElementTree(ET.fromstring(response.content))
+        root = tree.getroot()
+        for properties in root.findall('{http://www.w3.org/2005/Atom}entry/{http://www.w3.org/2005/Atom}content/{http://schemas.microsoft.com/ado/2007/08/dataservices/metadata}properties'):
+            items = [prop.text for prop in properties.iter()]
+            date = items[2]
+            yields = []
+            for line in items[3:-1]:
+                if line != None:
+                    yields.append(float((line)))
+                else:
+                    yields.append(None)
+            self.days[date] = yields
+
+    def getDate(self, date):
+        date = date.replace(hour=0, minute=0, second=0, microsecond=0)
+        date_str = date.strftime("%Y-%m-%dT%H:%M:%S")
+
+        # Try exact date first. If that fails, try previous day.
+        if date_str in self.days:
+           return self.days.get(date_str)
+        date_str = (date - timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:%S")
+        if date_str in self.days:
+           return self.days.get(date_str)
+        date_str = (date - timedelta(days=2)).strftime("%Y-%m-%dT%H:%M:%S")
+        if date_str in self.days:
+           return self.days.get(date_str)
+        date_str = (date - timedelta(days=3)).strftime("%Y-%m-%dT%H:%M:%S")
+        if date_str in self.days:
+           return self.days.get(date_str)
+        date_str = (date - timedelta(days=4)).strftime("%Y-%m-%dT%H:%M:%S")
+        if date_str in self.days:
+           return self.days.get(date_str)
+        return None
+
+    def queryData(self):
+        # Get dates
+        now = datetime.today()
+        pacific = now - timedelta(hours=8)
+        #central = now - timedelta(hours=6)
+        #eastern = now - timedelta(hours=5)
+        now = pacific
+        curves = [
+            ['Today'] + self.getDate(now),
+            ['Yesterday'] + self.getDate(now - timedelta(days=1)),
+            ['Last Week'] + self.getDate(now - timedelta(days=7)),
+            ['Last Month'] + self.getDate(now - timedelta(days=30)),
+            ['Three Months'] + self.getDate(now - timedelta(days=90)),
+            ['Six Months'] + self.getDate(now - timedelta(days=182)),
+            ['Last year'] + self.getDate(now - timedelta(days=365)),
+        ]
+        return curves
+
+    def higharts(self, curves):
+        ret = []
+        for day in curves:
+            ret.append({"name": day[0], "data": day[1:]})
+        return ret
+
+def lambda_handler(event, context):
+    year = datetime.today().year
+    y = YieldData()
+    #y.pullData('http://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=year(NEW_DATE)%20eq%202018')
+    y.pullData(f'http://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=year(NEW_DATE)%20eq%20{year-1}')
+    y.pullData(f'http://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=year(NEW_DATE)%20eq%20{year}')
+    curves = y.queryData()
+    data = y.higharts(curves)
+    print(data)
+    response = {
+        'statusCode': 200,
+        'body': json.dumps(data)
+    }
+    response["headers"] = {
+        'Content-Type': 'application/json', 
+        'Access-Control-Allow-Origin': '*' 
+    }
+    
+    return response
